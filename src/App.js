ef6f64155cf09c0a446438b2e6717cdc416e779a
pasta1205 <cse.24bcsi60@silicon.ac.in>
2026-08-04 11:04:17 +0530

    Initial push for React App

---
+++ src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "120px",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1>🚀 React App Successfully Deployed on Amazon EKS 🚀</h1>
+
+      <h2>CI/CD Pipeline using Jenkins + Docker + Kubernetes</h2>
+
+      <h3>Created by Partha</h3>
+    </div>
+  );
+}
+
+export default App;
